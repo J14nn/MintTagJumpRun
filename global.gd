db5146f61ff26672511d3ec1f9792226d4f3c5c0
J14nn <132164041+J14nn@users.noreply.github.com>
2025-07-21 11:20:54 +0200

    Update global.gd

--- global.gd
+++ global.gd
@@ -3,5 +3,5 @@ extends Node
 var klettert: bool = false
 var springt: bool = false
 var angreift: bool = false
-var getroffen: Node2D = null
+var gegner_getroffen: Array[Node2D] = []
 var tot: bool = false
