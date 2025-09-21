f40491eca164ab1abcc7a8c49d740b471eadb6c7
J14nn <132164041+J14nn@users.noreply.github.com>
2025-09-22 01:31:12 +0200

    Update Restart.cmd

--- Restart.cmd
+++ Restart.cmd
@@ -2,23 +2,13 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET "TARGET_FOLDER=%~dp0"
+IF "%TARGET_FOLDER:~-1%"=="\" SET "TARGET_FOLDER=%TARGET_FOLDER:~0,-1%"
 
-echo Deleting all contents of %TARGET_FOLDER%...
-rmdir /s /q "%TARGET_FOLDER%"
-mkdir "%TARGET_FOLDER%"
-
-SET "ZIP_FILE=%TARGET_FOLDER%\..\MintTag.zip"
-echo Extracting %ZIP_FILE% to %TARGET_FOLDER%...
-powershell -Command "Expand-Archive -Path '%ZIP_FILE%' -DestinationPath '%TARGET_FOLDER%' -Force"
-
-SET "JSON_FILE=%TARGET_FOLDER%\config.json"
+SET "SUBDIR=%TARGET_FOLDER%\.Wordhighlighter_Release"
+SET "JSON_FILE=%SUBDIR%\config.json"
 
 echo Updating JSON file...
-powershell -Command ^
-    "$json = Get-Content -Raw '%JSON_FILE%' | ConvertFrom-Json; ^
-     $json.WorkingFolder = '%TARGET_FOLDER%'; ^
-     $json.TessdataPath = Join-Path '%TARGET_FOLDER%' 'tessdata'; ^
-     $json | ConvertTo-Json -Compress | Set-Content '%JSON_FILE%'"
+powershell -Command "$json = Get-Content -Raw '%JSON_FILE%' | ConvertFrom-Json; $json.WorkingFolder = '%SUBDIR%'; $json.TessdataPath = Join-Path '%SUBDIR%' 'tessdata'; $json | ConvertTo-Json -Compress | Set-Content '%JSON_FILE%'"
 
 echo Done.
 pause
